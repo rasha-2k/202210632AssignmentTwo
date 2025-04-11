3d4562ad75fdd9e02989bd392516e033d5619932
rasha-2k <rasha.k.alsaleh@gmail.com>
2025-04-12 02:32:45 +0300

    add spaces between statement to ensure a readable code

--- SortExample.java
+++ SortExample.java
@@ -2,19 +2,24 @@ import java.util.*;
 
 public class SortExample {
     public static void main(String[] args) {
+
         // This is unsorted array
         Integer[] array1 = new Integer[] { 12, 13, 24, 10, 3, 6, 90, 70 };
         int array2[] = { 2, 6, 3, 5, 1 };
+
         // Let's sort using quick sort
         quickSort(array1, 0, array1.length - 1);
+
         // Verify sorted array
         System.out.println(Arrays.toString(array1));
         mergeSort(array2, array2.length - 1);
+
         // Verify sorted array
         System.out.println(Arrays.toString(array2));
     }
 
     public static void quickSort(Integer[] arr, int low, int high) {
+
         // check for empty or null array
         if (arr == null || arr.length == 0) {
             return;
@@ -22,9 +27,11 @@ public class SortExample {
         if (low >= high) {
             return;
         }
+
         // Get the pivot element from the middle of the list
         int middle = low + (high - low) / 2;
         int pivot = arr[middle];
+        
         // make left < pivot and right > pivot
         int i = low, j = high;
         while (i <= j) {
@@ -32,10 +39,12 @@ public class SortExample {
             while (arr[i] < pivot) {
                 i++;
             }
+
             // Check until all values on left side array are greater than pivot
             while (arr[j] > pivot) {
                 j--;
             }
+
             // Now compare values from both side of lists to see if they need swapping
             // After swapping move the iterator on both lists
             if (i <= j) {
@@ -44,6 +53,7 @@ public class SortExample {
                 j--;
             }
         }
+
         // Do same operation as above recursively to sort two sub arrays
         if (low < j) {
             quickSort(arr, low, j);
